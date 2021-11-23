1f85bf0ed743823d2bc4d7872cdf7c4c78562730
Jasur Tursunov <jasurtursunov@MacBook-Air-Jasur.local>
2021-11-23 04:43:36 +0300

    swagger completed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import os
-
-from api import UserLogIn
+from flask_swagger_ui import get_swaggerui_blueprint
+# from api import UserLogIn
 
 SECRET_KEY = os.urandom(32)
 CONFIG_NAME_MAPPER = {
@@ -13,8 +13,25 @@ CONFIG_NAME_MAPPER = {
 def create_app(config_name=None):
     app = Flask(__name__)
 
+
+    @app.route('/static/<path:path>')
+    def send_static(path):
+        return send_from_directory('static', path)
+
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name':"MedLight API"
+        }
+    )
+
+
     if config_name is None:
         config_name = "production"
+
 
     app.config.from_object(CONFIG_NAME_MAPPER[config_name])
     app.config['SECRET_KEY'] = SECRET_KEY
@@ -30,4 +47,5 @@ def create_app(config_name=None):
     # Register Blueprints
     from api import api_bp
     app.register_blueprint(api_bp, url_prefix="/api/v1")
+    app.register_blueprint(swaggerui_blueprint, url_prefix="/api/v1" + SWAGGER_URL)
     return app
